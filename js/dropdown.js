a9b72e870800801adea62892295d3b6a101cd979
nocbersamajsn <94597338+nocbersamajsn@users.noreply.github.com>
2022-01-10 21:10:17 +0700

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -122,7 +122,7 @@ var dataEmail = [
     ['XL Axiata','Lamongan','Lamongan','004C6714L7','LAMONGAN','1 Gbps','aries.effendy@gmail.com',''],
     ['XL Axiata','Semarang','Semarang','020C6714L9','SEMARANG','500 Mbps','ragil@jsn.net.id',''],
     ['XL Axiata','Probolinggo','Probolinggo','004C6714L8','PROBOLINGGO','1 Gbps','hasenda14071986@gmail.com',''],
-    ['XL Axiata','Bengkalis','Bengkalis','020C6714L10','BENGKALIS','1 Gbps','wardiyono@rifansi.co.id',''],
+    ['XL Axiata','Bengkalis','Bengkalis','020C6714L10','BENGKALIS','1 Gbps','usman.malik@jsn.net.id',''],
     ['XL Axiata','Tuban','Tuban','004C6714L11','TUBAN','1 Gbps','rizal.abdi@gmail.com',''],
 
     // Hutcison Tri (H3I)
