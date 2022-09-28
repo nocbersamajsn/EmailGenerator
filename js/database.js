e3a4d4380326b9be68f71325f3a9df71349f388b
nocbersamajsn <94597338+nocbersamajsn@users.noreply.github.com>
2022-09-28 20:11:21 +0700

    Update database.js

--- js/database.js
+++ js/database.js
@@ -80,7 +80,7 @@ var dataEmail = [
     ['Icon Plus','Tulungagung','Sumbergempol','01000142915 (Metro-E)','Jln. Desa RT.01/RW.01 Somoteleng, Podorejo, Kec. Sumbergempol, Kabupaten Tulungagung, Jawa Timur, Indonesia (1 Gbps) (01000142915)','1 Gbps','badrayana@jsn.net.id','andromaxv320@gmail.com'],
     ['Icon Plus','Tulungagung','Tamban Pakel','01000143386 (IP VPN)','Lintas Barat Bungkur, Bungkur, Tamban Pakel, Kab.Tulungagung, Jawa Timur 66273, Indonesia (100Mbps) (01000143386)','100 Mbps','badrayana@jsn.net.id',''],
     ['Icon Plus','Malang','Bantur','01000152771 (Metro-E)','Dusun Bantur Timur RT33/RW 07, Desa Bantur Kec Bantur, Kabupaten Malang ,Jawa Timur ,Indonesia (1 Gbps) (01000152771)','1 Gbps','malang@jsn.net.id',''],
-    ['Icon Plus','Malang','Pagak','01000300143 (Metro-E)','Dusun Judeg, RT.10/RW.03, Desa Tlogorejo, Kecamatan Pagak, Kabupaten Malang, Jawa Timur (2 Gbps) (01000300143)','1 Gbps','malang@jsn.net.id',''],
+    ['Icon Plus','Malang','Pagak','01000300143 (Metro-E)','Dusun Judeg, RT.10/RW.03, Desa Tlogorejo, Kecamatan Pagak, Kabupaten Malang, Jawa Timur (2 Gbps) (01000300143)','5 Gbps','malang@jsn.net.id',''],
     ['Icon Plus','Malang','Ampel Gading','01000189483 (Metro-E)','Jl. Raya Tirtomarto, Putukrejo, Tirtomarto, Ampelgading, Kabupaten Malang. Jawa Timur 65183, lndonesia. (1 Gbps) (01000189483)','1 Gbps','malang@jsn.net.id',''],
     ['Icon Plus','Malang','Singosari','01000307552 (Metro-E)','Krajan, Dengkol, Kec.Singosari, Malang Kab. Malang, Jawa Tmur 65153, Indonesia (1 Gbps) (01000307552)','1 Gbps','malang@jsn.net.id',''],
     ['Icon Plus','Malang','Bantur','01000143366 (Metro-E)','Balewarti. rejosari, Bantur, Malang, Kab.Malang, Jawa Timur 65179, Indonesia (1 Gbps) (01000143366)','1 Gbps','malang@jsn.net.id',''],
